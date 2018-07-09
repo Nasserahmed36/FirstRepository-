53d1d95525750bcd01553756898950afdc61a194
Nasser Alkhateeb <nasserahmed36@gmail.com>
2018-07-09 13:19:24 +0300

    Seccond commit

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+ JFrame.setDefaultLookAndFeelDecorated(true);
+    JFrame frame = new JFrame();
+    frame.setTitle("My First Swing Application");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JLabel label = new JLabel("Welcome");
+    frame.add(label);
+    frame.pack();
+    frame.setVisible(true);
+    }
+}
